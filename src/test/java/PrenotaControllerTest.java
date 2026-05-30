f5d6fd2d8a356e8a0bdc8696e5539cf7c4161c53
Simone <simo.giova2024@gmail.com>
2026-05-30 19:10:38 +0200

    modifiche per testing

---
+++ src/test/java/PrenotaControllerTest.java
@@ -0,0 +1,15 @@
+import com.example.skatingplan.controllerapplicativi.PrenotaController;
+import com.example.skatingplan.model.bean.FiltriBean;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+class PrenotaControllerTest {
+
+    @Test
+    void selezionaLezioniTest() {
+        PrenotaController prenotaController = new PrenotaController();
+        FiltriBean filtriBeanMock = mock(FiltriBean.class);
+
+    }
+}
