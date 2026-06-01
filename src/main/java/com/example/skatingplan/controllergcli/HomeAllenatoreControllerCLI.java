274fc0b979bd8e9d053cc476147142ad42b1704c
Simone <simo.giova2024@gmail.com>
2026-06-01 22:02:21 +0200

    modifiche per testing

--- src/main/java/com/example/skatingplan/controllergcli/HomeAllenatoreControllerCLI.java
+++ src/main/java/com/example/skatingplan/controllergcli/HomeAllenatoreControllerCLI.java
@@ -1,14 +1,5 @@
 package com.example.skatingplan.controllergcli;
 
-import com.example.skatingplan.controllergcli.viewcli.HomeAtletaViewCLI;
-import com.example.skatingplan.eccezioni.DatabaseNonRaggiungibileException;
-import com.example.skatingplan.eccezioni.InputIllegaleException;
-import com.example.skatingplan.model.dao.dbms.DBMSFactory;
-import com.example.skatingplan.model.enumerazioni.Role;
-import com.example.skatingplan.utili.ConnectionFactory;
-import com.example.skatingplan.utili.FactoryConfig;
-
-import java.io.IOException;
 
 public class HomeAllenatoreControllerCLI {
 
@@ -17,53 +8,8 @@ public class HomeAllenatoreControllerCLI {
     }
 
     public static void start() {
-        boolean continua = true;
-
-        while (continua) {
-            try {
-                int choice;
-
-                choice = selezionascelta();
-
-                switch (choice) {
-                    case 1 -> {
-                        //non implementata
-                    }
-                    case 2 -> {
-                        //da implementare
-                    }
-                    case 3 -> {
-                        logout();
-                        continua = false;
-                    }
-                    default -> throw new InputIllegaleException("Scelta non valida, riprovare");
-
-                }
-            } catch (InputIllegaleException e) {
-                HomeAtletaViewCLI.mostraErrore(e.getMessage());
-            }
-        }
+        //da implementare
     }
 
-    private static int selezionascelta() throws InputIllegaleException {
-        try {
-            return HomeAtletaViewCLI.mostraOperazioni();
-        } catch (IOException e) {
-            throw new InputIllegaleException("Input non valido, riprovare", e);
-        }
-    }
-
-
-    private static void logout() {
-        try {
-            //resetto la connessione per fare il login
-            if (FactoryConfig.getDaoFactory() instanceof DBMSFactory) {
-                ConnectionFactory.changeRole(Role.LOGIN);
-            }
-
-        } catch (DatabaseNonRaggiungibileException e) {
-            HomeAtletaViewCLI.mostraErrore(e.getMessage());
-        }
-    }
 }
 
--- src/main/java/com/example/skatingplan/controllergcli/HomeManagerControllerCLI.java
+++ src/main/java/com/example/skatingplan/controllergcli/HomeManagerControllerCLI.java
@@ -1,14 +1,5 @@
 package com.example.skatingplan.controllergcli;
 
-import com.example.skatingplan.controllergcli.viewcli.HomeAtletaViewCLI;
-import com.example.skatingplan.eccezioni.DatabaseNonRaggiungibileException;
-import com.example.skatingplan.eccezioni.InputIllegaleException;
-import com.example.skatingplan.model.dao.dbms.DBMSFactory;
-import com.example.skatingplan.model.enumerazioni.Role;
-import com.example.skatingplan.utili.ConnectionFactory;
-import com.example.skatingplan.utili.FactoryConfig;
-
-import java.io.IOException;
 
 public class HomeManagerControllerCLI {
 
@@ -17,52 +8,8 @@ public class HomeManagerControllerCLI {
     }
 
     public static void start(){
-        boolean continua = true;
-
-        while (continua) {
-            try {
-                int choice;
-
-                choice = selezionascelta();
-
-                switch (choice) {
-                    case 1 -> {
-                        //non implementata
-                    }
-                    case 2 -> {
-                        //da implementare
-                    }
-                    case 3 -> {
-                        logout();
-                        continua = false;
-                    }
-                    default -> throw new InputIllegaleException("Scelta non valida, riprovare");
-
-                }
-            }catch (InputIllegaleException  e) {
-                HomeAtletaViewCLI.mostraErrore(e.getMessage());
-            }
-        }
-    }
-
-    private static int selezionascelta() throws InputIllegaleException {
-        try {
-            return HomeAtletaViewCLI.mostraOperazioni();
-        }catch (IOException e) {
-            throw new InputIllegaleException("Input non valido, riprovare", e);
-        }
+       //da implementare
     }
 
 
-    private static void logout(){
-        try {
-            //resetto la connessione per fare il login
-            if(FactoryConfig.getDaoFactory() instanceof DBMSFactory){
-                ConnectionFactory.changeRole(Role.LOGIN);
-            }
-
-        }catch (DatabaseNonRaggiungibileException e){
-            HomeAtletaViewCLI.mostraErrore(e.getMessage());
-        }
-    }
 }
